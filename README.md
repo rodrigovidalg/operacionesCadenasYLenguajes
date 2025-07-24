918980da01fe12e6f536b1fb5fbc10f14f3bf84a
RodrigoVidalGuzmanDiaz <rodrigovidalguzmanduaz1@gmail.com>
2025-07-23 21:52:37 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# operacionesCadenasYLenguajes
+# operacionesCadenasYLenguajes
---
+++ src/main/java/com/mycompany/operacionescadenasylenguajes/OperacionesCadenasYLenguajes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operacionescadenasylenguajes;
+
+/**
+ *
+ * @author user
+ */
+public class OperacionesCadenasYLenguajes {
+
+    public static void main(String[] args) {
+        frm_cadenasylenguajes frm = new frm_cadenasylenguajes();
+        frm.show();
+    }
+}
